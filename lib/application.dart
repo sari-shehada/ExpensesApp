e296fa345ac014cce80a0b27cb4d72c7cd7ce030
Sari Shehada <sarishehada@gmail.com>
2024-12-18 15:25:03 +0400

    Initial Commit

---
+++ lib/application.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Welcome to Expenses"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:expenses_app/application.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Application(),
+  );
+}
